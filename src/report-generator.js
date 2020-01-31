c947345246d6d33ee74fc6bc4daf90455cd5f07c
Chris Collins <chris@performantsoftware.com>
2020-01-31 11:28:51 -0500

    WIP 5 array of gh uri's created

--- src/report-generator.js
+++ src/report-generator.js
@@ -7,53 +7,59 @@ var jade = require('jade');
 var rp = require('request-promise');
 var Buffer = require('buffer').Buffer;
 
-function getReportName(repo, owner) {
-  return [
-    owner,
-    '_',
-    repo,
-    '-',
-    dateFormat(new Date(), 'yyyymdd-HHMMss'),
-    '.html'
-  ].join('');
+function generateAuthorization(token) {
+    return 'Basic ' + new Buffer(token + ':x-oauth-basic').toString('base64');
 }
-
 
 module.exports = {
   generate: (config) => {
 
-    function generateAuthorization(token) {
-        return 'Basic ' + new Buffer(token + ':x-oauth-basic').toString('base64');
-    }
+      const generateAuthorization = (token) => {
+          return 'Basic ' + new Buffer(token + ':x-oauth-basic').toString('base64');
+      };
 
-    const zhOptions = {
-        uri: 'https://api.zenhub.com/p1/reports/release/5e309e4032970b6af25e726c/issues',
-        headers: {
-            'X-Authentication-Token': config.ztoken
-        }
-    };
+      const zhOptions = {
+          uri: 'https://api.zenhub.com/p1/reports/release/5e309e4032970b6af25e726c/issues',
+          headers: {
+              'X-Authentication-Token': config.ztoken
+          }
+      };
 
-    const ghOptions = {
-        // TODO: the repo ID & issue # needs to be determined dynamically
-        uri: 'https://api.github.com/repos/performant-software/3dfleet-client-lib/issues',
-        headers: {
-            'User-Agent': 'github-issue-reports',
-            'Authentication': config.gtoken
-        }
-    }
+      const ghOptions = {
+          // TODO: the repo ID & issue # needs to be determined dynamically
+          uri: 'https://api.github.com/repos/performant-software/3DFleet/issues/',
+          headers: {
+              'User-Agent': 'github-issue-reports',
+          }
+      };
+      const getGhOptions = issueNumber => {
+          uri: ghOptions.uri + issueNumber
+      }
 
-    rp(zhOptions)
-        .then(results => {
-            let parsedResults = JSON.parse(results)
-            console.log('1st then')
-            console.log(parsedResults)
-            return rp(ghOptions)
-                .then(details => {
-                    console.log(details)
-                })
+
+      if (config.gtoken) {
+          ghOptions.headers['Authorization'] = generateAuthorization(config.gtoken);
+      }
+
+      rp(zhOptions)
+          .then(results => {
+            let parsedResults = JSON.parse(results);
+            let issuesNumbers = parsedResults.map(issue => issue.issue_number);
+            console.log('1st then');
+            console.log(parsedResults);
+            console.log(issuesNumbers);
+            let ghUriIssueList = issuesNumbers.map((number, i) => {
+                let ghOptionsCopy = {...ghOptions};
+                return ghOptionsCopy.uri = ghOptionsCopy.uri + issuesNumbers[i]
+            });
+              console.log(ghUriIssueList);
+            // return rp(ghOptions)
         })
-        .catch(function (err) {
-          console.log("ERROR!!")
+          .then(details => {
+          console.log(JSON.parse(details))
+        })
+          .catch(function (err) {
+          console.log("ERROR!!");
           console.log(err)
         });
 
